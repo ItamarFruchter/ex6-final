e48efb0fff7c90dc5d4fe9a3f4b50ae850afbf5a
T8143777 <T8143777@talpiot132.talpiot.huji.ac.il>
2016-06-14 20:27:17 +0300

    now it is done

--- src/oop/ex6/blockAnalayzer/MethodBlock.java
+++ src/oop/ex6/blockAnalayzer/MethodBlock.java
@@ -12,55 +12,6 @@ import oop.ex6.variables.Type;
  * this class represents a method block
  */
 public class MethodBlock extends Block {
-
-	/*
-	private class Argument {
-
-		private Type typeOfArgument;
-		private String nameOfArgument;
-
-		private Argument(String type, String name) throws IllegalCodeException {
-			this.typeOfArgument = Type.findType(type);
-			this.nameOfArgument = checkName(name);
-		}
-	}
-	*/
-	
-	private enum MethodBlockType {
-		VOID("void");
-
-		// The string representation of this method type.
-		private String stringRepresentation;
-
-		// A constructor.
-		private MethodBlockType(String stringRepresentation) {
-			this.stringRepresentation = stringRepresentation;
-		}
-
-		private String getRepresentation() {
-			return stringRepresentation;
-		}
-
-		/**
-		 * Returns the non-method block type fitting for the given string.
-		 * Throws an exception if non exist.
-		 * 
-		 * @param stringRepresentation
-		 *            The requested type's string representation.
-		 * @return The non-method block type fitting for this string
-		 *         representation, if exists.
-		 * @throws IllegalCodeException
-		 */
-		public static MethodBlockType methodTypeFromString(String stringRepresentation)
-				throws IllegalCodeException {
-			for (MethodBlockType nonMethodType : MethodBlockType.values()) {
-				if (nonMethodType.getRepresentation().equals(stringRepresentation)) {
-					return nonMethodType;
-				}
-			}
-			throw new UnknownBlockTypeException();
-		}
-	}
 	private static final Pattern NAME_PATTERN = Pattern.compile("[a-zA-Z]\\w*");
 
 	private String name;
@@ -73,5 +24,12 @@ public class MethodBlock extends Block {
 				localMembers.addAll(MemberFactory.createMembers(argument));
 			}
 		}
+	}
+
+	@Override
+	public void checkContent(MethodBlock[] knownMethods)
+			throws IllegalCodeException {
+		// TODO Auto-generated method stub
+		
 	}
 }
