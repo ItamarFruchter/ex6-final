a6d76101e599fd3c9a5697f0a556d0e1b6244b81
Ohad Ben-Or <ohadbenor1996@gmail.com>
2016-06-15 01:14:53 +0300

    new exception

---
+++ src/oop/ex6/variables/MemberAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package oop.ex6.variables;
+
+import oop.ex6.error.IllegalCodeException;
+
+public class MemberAlreadyExistsException extends IllegalCodeException {
+
+	private static final long serialVersionUID = 1L;
+
+}
