adbaf93b05feb03d1f80dcb9e48584463245b8ad
T8143777 <T8143777@talpiot132.talpiot.huji.ac.il>
2016-06-14 20:44:23 +0300

    stam push

--- src/oop/ex6/variables/MemberFactory.java
+++ src/oop/ex6/variables/MemberFactory.java
@@ -13,59 +13,67 @@ public class MemberFactory {
 	private static final char SPACE = ' ';
 	private static final char SEMICOLON = ';';
 	private static final Pattern WORD = Pattern.compile("\\w+");
-	private static final Pattern WITH_MODIFIER = Pattern.compile("\\w+\\s+\\w+\\s+\\w+");
+	private static final Pattern WITH_MODIFIER = Pattern
+			.compile("\\w+\\s+\\w+\\s+\\w+");
 	private static final int MEMBER_VALUE = 1;
 	private static final int MEMBER_NAME = 0;
-	
+
 	/*
 	 * this class represents an object that holds the member name and value
 	 */
-	private static class MemberParameters{
-		
+	private static class MemberParameters {
+
 		private String name;
 		private String value;
-		
+
 		/**
 		 * constructs the member parameters
-		 * @param name - the name of the object
-		 * @param value - the value of the object
+		 * 
+		 * @param name
+		 *            - the name of the object
+		 * @param value
+		 *            - the value of the object
 		 */
-		private MemberParameters(String name, String value){
+		private MemberParameters(String name, String value) {
 			this.name = name;
 			this.value = value;
 		}
 	}
-	
-	private MemberFactory(){}
-	
+
+	private MemberFactory() {
+	}
+
 	/**
-	 * @param line - the line we want generate members from
-	 * @return an array of the members
+	 * @param line
+	 *            the line we want generate members from.
+	 * @return an array of the members.
 	 * @throws IllegalCodeException
 	 */
-	public static LinkedList<Member> createMembers(String line) throws IllegalCodeException{
+	public static LinkedList<Member> createMembers(String line)
+			throws IllegalCodeException {
 		String tempString = new String(line);
 		tempString = tempString.trim();
 		Matcher withModifireMatcher = WITH_MODIFIER.matcher(tempString);
-		if (withModifireMatcher.lookingAt()){
+		if (withModifireMatcher.lookingAt()) {
 			return createMembersWithModifire(tempString);
-		}
-		else {
+		} else {
 			return createMembersWithoutModifire(tempString);
 		}
-	}	
-	
+	}
+
 	/*
 	 * handles the case there is a modifier to the objects
 	 */
-	private static LinkedList<Member> createMembersWithoutModifire(String tempString) throws IllegalCodeException {
+	private static LinkedList<Member> createMembersWithoutModifire(
+			String tempString) throws IllegalCodeException {
 		LinkedList<Member> listOfMembers = new LinkedList<Member>();
 		Matcher wordMatcher = WORD.matcher(tempString);
 		wordMatcher.find();
-		String type = tempString.substring(wordMatcher.start(), wordMatcher.end());
+		String type = tempString.substring(wordMatcher.start(),
+				wordMatcher.end());
 		tempString = tempString.substring(wordMatcher.end());
 		MemberParameters[] members = extractMembers(tempString);
-		for (MemberParameters member : members ){
+		for (MemberParameters member : members) {
 			listOfMembers.add(new Member(member.name, type, member.value));
 		}
 		return listOfMembers;
@@ -74,41 +82,45 @@ public class MemberFactory {
 	/*
 	 * handles the case there is no modifier
 	 */
-	private static LinkedList<Member> createMembersWithModifire(String tempString) throws IllegalCodeException {
+	private static LinkedList<Member> createMembersWithModifire(
+			String tempString) throws IllegalCodeException {
 		LinkedList<Member> listOfMembers = new LinkedList<Member>();
 		Matcher wordMatcher = WORD.matcher(tempString);
 		wordMatcher.find();
-		String modifier = tempString.substring(wordMatcher.start(), wordMatcher.end());
+		String modifier = tempString.substring(wordMatcher.start(),
+				wordMatcher.end());
 		tempString = tempString.substring(wordMatcher.end());
 		wordMatcher.reset(tempString);
 		wordMatcher.find();
-		String type = tempString.substring(wordMatcher.start(), wordMatcher.end());
+		String type = tempString.substring(wordMatcher.start(),
+				wordMatcher.end());
 		tempString = tempString.substring(wordMatcher.end());
 		MemberParameters[] members = extractMembers(tempString);
-		for (MemberParameters member : members ){
-			listOfMembers.add(new Member(member.name, type, member.value, modifier));
+		for (MemberParameters member : members) {
+			listOfMembers
+					.add(new Member(member.name, type, member.value, modifier));
 		}
 		return listOfMembers;
 	}
-	
-	
+
 	/*
 	 * generate an array of MemberParameter from the line given
 	 */
 	private static MemberParameters[] extractMembers(String tempString) {
 		LinkedList<MemberParameters> members = new LinkedList<MemberParameters>();
 		String[] membersString = tempString.split(",");
-		for (int i = 0; i < membersString.length; i++){
-			if (i == membersString.length-1){
+		for (int i = 0; i < membersString.length; i++) {
+			if (i == membersString.length - 1) {
 				// removes the semicolon from the end of the line
 				membersString[i] = membersString[i].replace(SEMICOLON, SPACE);
 			}
 			String[] parameters = membersString[i].split("=");
-			if (parameters.length == 2){
-				members.add(new MemberParameters(parameters[MEMBER_NAME], parameters[MEMBER_VALUE]));
-			}
-			else {
-				members.add(new MemberParameters(parameters[MEMBER_NAME], null));
+			if (parameters.length == 2) {
+				members.add(new MemberParameters(parameters[MEMBER_NAME],
+						parameters[MEMBER_VALUE]));
+			} else {
+				members.add(
+						new MemberParameters(parameters[MEMBER_NAME], null));
 			}
 		}
 		return members.toArray(new MemberParameters[members.size()]);
