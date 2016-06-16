857b8dd2fce8ee3de5dcfa35d4b0ee6bcc556443
Ohad Ben-Or <ohadbenor1996@gmail.com>
2016-06-16 11:22:59 +0300

    readme

--- src/oop/ex6/blockAnalayzer/UnknownMemberNameException.java
+++ src/oop/ex6/blockAnalayzer/UnknownMemberNameException.java
@@ -5,7 +5,7 @@ import oop.ex6.general.IllegalCodeException;
 /**
  * Arises whenever an unknown member is accessed through assignment.
  */
-public class UnknownMemberNameException extends IllegalCodeException {
+class UnknownMemberNameException extends IllegalCodeException {
 
 	private static final long serialVersionUID = 1L;
 	private static final String MESSAGE = "The variable we want to assign does not exists.";
